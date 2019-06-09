7f694d1b0a4a182d9b79df0056155b74fff7a8b9
Brianna Garcia <briannagarcia@Briannas-MacBook-Pro.local>
2019-06-09 12:23:10 -0600

    Added hover animations and links to portfolio items

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
-const navSlide = () => {
-  const burger = document.querySelector('.burger');
-  const nav = document.querySelector('.nav-links');
+$(function() {
+  const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+  
+    burger.addEventListener('click',()=>{
+      nav.classList.toggle('nav-active');
+    });
+  }
+  
+  navSlide();
 
-  burger.addEventListener('click',()=>{
-    nav.classList.toggle('nav-active');
+  $('.cover').on('mouseover', function() {
+    $overlay = $(this).children('.overlay');
+    if (!$overlay.hasClass('overlay-animation')) {
+      $overlay.addClass('overlay-animation');
+    }
   });
-}
 
-navSlide();
-
-
-
+  $('.cover').on('mouseleave', function() {
+    $overlay = $(this).children('.overlay');
+    if ($overlay.hasClass('overlay-animation')) {
+      $overlay.removeClass('overlay-animation');
+    }
+  });
+});
